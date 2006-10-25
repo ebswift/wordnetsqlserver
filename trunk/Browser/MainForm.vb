4282176870aaa563dbf221d61602801b414bcfcc
ebswift <ebswift@db133920-4420-0410-b7f4-e132a4aa95df>
2006-10-25 00:19:47 +0000

    Added required SharpDevelop external sources

---
+++ trunk/Browser/MainForm.vb
@@ -0,0 +1,18 @@
+'
+' Created by SharpDevelop.
+' User: simpsont
+' Date: 25/10/2006
+' Time: 9:47 AM
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+Public Partial Class MainForm
+	Public Sub New()
+		' The Me.InitializeComponent call is required for Windows Forms designer support.
+		Me.InitializeComponent()
+		
+		'
+		' TODO : Add constructor code after InitializeComponents
+		'
+	End Sub
+End Class
