fc4a12f61801100938662792d000448b00379ec4
ebswift <ebswift@db133920-4420-0410-b7f4-e132a4aa95df>
2006-10-25 01:30:53 +0000

    Got the SharpDevelop texteditor to compile correctly.

---
+++ trunk/src/Tools/UpdateAssemblyInfo/Main.vb
@@ -0,0 +1,14 @@
+'
+' Created by SharpDevelop.
+' User: simpsont
+' Date: 25/10/2006
+' Time: 11:17 AM
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+Module Main
+	Public Function Main() As Integer
+		Console.WriteLine("This application Is a dummy placeholder For SharpDevelop's UpdateAssemblyInfo")
+		Return 0
+	End Function
+End Module
