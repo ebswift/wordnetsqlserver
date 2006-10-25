19248269bf98a8c56a2eb85a762f3bdb9a6d9782
ebswift <ebswift@db133920-4420-0410-b7f4-e132a4aa95df>
2006-10-25 23:15:10 +0000

    SQL editor now working, added a new browserdata database for storing SQL statements.  Made it separate so that the huge wordnet database does not have to be fully committed after every minute change.

--- trunk/Browser/MainForm.Designer.vb
+++ trunk/Browser/MainForm.Designer.vb
@@ -1,4 +1,4 @@
-'
+﻿'
 ' Created by SharpDevelop.
 ' User: simpsont
 ' Date: 25/10/2006
@@ -33,11 +33,136 @@ Partial Class MainForm
 	''' not be able to load this method if it was changed manually.
 	''' </summary>
 	Private Sub InitializeComponent()
+		Me.sqlEditor = New ICSharpCode.TextEditor.TextEditorControl
+		Me.tvPresets = New System.Windows.Forms.TreeView
+		Me.dataGridView1 = New System.Windows.Forms.DataGridView
+		Me.dataSet1 = New System.Data.DataSet
+		Me.sqlConnection1 = New System.Data.SqlClient.SqlConnection
+		Me.sqlDataAdapter1 = New System.Data.SqlClient.SqlDataAdapter
+		Me.btnExecute = New System.Windows.Forms.Button
+		Me.grpPresets = New System.Windows.Forms.GroupBox
+		Me.grpSQL = New System.Windows.Forms.GroupBox
+		Me.grpOutput = New System.Windows.Forms.GroupBox
+		Me.dataSet2 = New System.Data.DataSet
+		Me.daPreset = New System.Data.SqlClient.SqlDataAdapter
+		CType(Me.dataGridView1,System.ComponentModel.ISupportInitialize).BeginInit
+		CType(Me.dataSet1,System.ComponentModel.ISupportInitialize).BeginInit
+		Me.grpPresets.SuspendLayout
+		Me.grpSQL.SuspendLayout
+		Me.grpOutput.SuspendLayout
+		CType(Me.dataSet2,System.ComponentModel.ISupportInitialize).BeginInit
+		Me.SuspendLayout
+		'
+		'sqlEditor
+		'
+		Me.sqlEditor.IsIconBarVisible = false
+		Me.sqlEditor.Location = New System.Drawing.Point(6, 20)
+		Me.sqlEditor.Name = "sqlEditor"
+		Me.sqlEditor.ShowEOLMarkers = true
+		Me.sqlEditor.ShowInvalidLines = false
+		Me.sqlEditor.ShowSpaces = true
+		Me.sqlEditor.ShowTabs = true
+		Me.sqlEditor.ShowVRuler = true
+		Me.sqlEditor.Size = New System.Drawing.Size(433, 212)
+		Me.sqlEditor.TabIndex = 0
+		'
+		'tvPresets
+		'
+		Me.tvPresets.Location = New System.Drawing.Point(6, 18)
+		Me.tvPresets.Name = "tvPresets"
+		Me.tvPresets.Size = New System.Drawing.Size(188, 244)
+		Me.tvPresets.TabIndex = 1
+		'
+		'dataGridView1
+		'
+		Me.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+		Me.dataGridView1.Location = New System.Drawing.Point(6, 20)
+		Me.dataGridView1.Name = "dataGridView1"
+		Me.dataGridView1.ReadOnly = true
+		Me.dataGridView1.Size = New System.Drawing.Size(639, 148)
+		Me.dataGridView1.TabIndex = 2
+		'
+		'dataSet1
+		'
+		Me.dataSet1.DataSetName = "NewDataSet"
+		'
+		'sqlConnection1
+		'
+		Me.sqlConnection1.FireInfoMessageEventOnUserErrors = false
+		'
+		'btnExecute
+		'
+		Me.btnExecute.Location = New System.Drawing.Point(6, 239)
+		Me.btnExecute.Name = "btnExecute"
+		Me.btnExecute.Size = New System.Drawing.Size(75, 23)
+		Me.btnExecute.TabIndex = 3
+		Me.btnExecute.Text = "E&xecute"
+		Me.btnExecute.UseVisualStyleBackColor = true
+		AddHandler Me.btnExecute.Click, AddressOf Me.BtnExecuteClick
+		'
+		'grpPresets
+		'
+		Me.grpPresets.Controls.Add(Me.tvPresets)
+		Me.grpPresets.Location = New System.Drawing.Point(12, 12)
+		Me.grpPresets.Name = "grpPresets"
+		Me.grpPresets.Size = New System.Drawing.Size(200, 268)
+		Me.grpPresets.TabIndex = 4
+		Me.grpPresets.TabStop = false
+		Me.grpPresets.Text = "Presets"
+		'
+		'grpSQL
+		'
+		Me.grpSQL.Controls.Add(Me.sqlEditor)
+		Me.grpSQL.Controls.Add(Me.btnExecute)
+		Me.grpSQL.Location = New System.Drawing.Point(218, 12)
+		Me.grpSQL.Name = "grpSQL"
+		Me.grpSQL.Size = New System.Drawing.Size(445, 268)
+		Me.grpSQL.TabIndex = 5
+		Me.grpSQL.TabStop = false
+		Me.grpSQL.Text = "SQL Editor"
+		'
+		'grpOutput
+		'
+		Me.grpOutput.Controls.Add(Me.dataGridView1)
+		Me.grpOutput.Location = New System.Drawing.Point(12, 286)
+		Me.grpOutput.Name = "grpOutput"
+		Me.grpOutput.Size = New System.Drawing.Size(651, 174)
+		Me.grpOutput.TabIndex = 6
+		Me.grpOutput.TabStop = false
+		Me.grpOutput.Text = "Output"
+		'
+		'dataSet2
+		'
+		Me.dataSet2.DataSetName = "NewDataSet"
 		'
 		'MainForm
 		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(675, 472)
+		Me.Controls.Add(Me.grpOutput)
+		Me.Controls.Add(Me.grpSQL)
+		Me.Controls.Add(Me.grpPresets)
 		Me.Name = "MainForm"
 		Me.Text = "Browser"
+		CType(Me.dataGridView1,System.ComponentModel.ISupportInitialize).EndInit
+		CType(Me.dataSet1,System.ComponentModel.ISupportInitialize).EndInit
+		Me.grpPresets.ResumeLayout(false)
+		Me.grpSQL.ResumeLayout(false)
+		Me.grpOutput.ResumeLayout(false)
+		CType(Me.dataSet2,System.ComponentModel.ISupportInitialize).EndInit
+		Me.ResumeLayout(false)
 	End Sub
+	Private daPreset As System.Data.SqlClient.SqlDataAdapter
+	Private dataSet2 As System.Data.DataSet
+	Private tvPresets As System.Windows.Forms.TreeView
+	Private grpOutput As System.Windows.Forms.GroupBox
+	Private grpSQL As System.Windows.Forms.GroupBox
+	Private grpPresets As System.Windows.Forms.GroupBox
+	Private btnExecute As System.Windows.Forms.Button
+	Private sqlDataAdapter1 As System.Data.SqlClient.SqlDataAdapter
+	Private dataSet1 As System.Data.DataSet
+	Private sqlConnection1 As System.Data.SqlClient.SqlConnection
+	Private dataGridView1 As System.Windows.Forms.DataGridView
+	Private sqlEditor As ICSharpCode.TextEditor.TextEditorControl
 End Class
--- trunk/Browser/MainForm.vb
+++ trunk/Browser/MainForm.vb
@@ -1,3 +1,4 @@
+Imports System.IO
 '
 ' Created by SharpDevelop.
 ' User: simpsont
@@ -7,6 +8,8 @@
 ' To change this template use Tools | Options | Coding | Edit Standard Headers.
 '
 Public Partial Class MainForm
+	Public quot As String = Chr(34)
+	
 	Public Sub New()
 		' The Me.InitializeComponent call is required for Windows Forms designer support.
 		Me.InitializeComponent()
@@ -14,5 +17,35 @@ Public Partial Class MainForm
 		'
 		' TODO : Add constructor code after InitializeComponents
 		'
+        Dim appPath As String = Path.GetDirectoryName(Application.ExecutablePath)
+        Dim provider As ICSharpCode.TextEditor.Document.FileSyntaxModeProvider = New ICSharpCode.TextEditor.Document.FileSyntaxModeProvider(appPath)
+        ICSharpCode.TextEditor.Document.HighlightingManager.Manager.AddSyntaxModeFileProvider(provider)
+        sqlEditor.Document.HighlightingStrategy = ICSharpCode.TextEditor.Document.HighlightingManager.Manager.FindHighlighter("SQL")
+
+		Me.SqlConnection1.ConnectionString = "Data Source=.\SQLEXPRESS;AttachDbFilename=" & quot & _
+				Data_Path() & _
+				quot & ";Integrated Security=True;User Instance=False;Timeout=60"
+	End Sub
+
+	Public Readonly Property Data_Path() As String
+		Get
+			'MessageBox.Show(Application.StartupPath & "..\..\..\Database\wordnet.mdf")
+			Return "D:\TroyDevel\Proj\WordNetSQLServer\Database\wordnet.mdf"
+'			Return Application.StartupPath & "\..\..\..\Database\wordnet.mdf"
+		End Get
+	End Property
+	
+	
+	Sub BtnExecuteClick(sender As Object, e As System.EventArgs)
+		Dim sc As New System.Data.SqlClient.SqlCommand(sqlEditor.Text, sqlconnection1)
+		
+		dataset1.Tables.Clear()
+		sqlDataAdapter1.SelectCommand = sc
+		sqlDataAdapter1.Fill(dataset1)
+		datagridview1.DataSource = dataset1.Tables(0)
+		
+'		MessageBox.Show(dataset1.Tables(0).Rows(0)(1))
+'		datagridview1.DataMember = dataset1.Tables(0).TableName
+		datagridview1.Refresh
 	End Sub
 End Class
